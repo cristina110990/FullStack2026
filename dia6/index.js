b124fd49dd8be15905943ac09e6230f8ef69ec06
Cristina Villar Romero <cristina110990@gmail.com>
2026-01-27 17:27:43 +0100

    dia6

---
+++ dia6/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const usersRouter = require('./routers/usersRouters');
+
+const app = express();
+const PORT = 3000;
+const MONGO_URI = 'mongodb://localhost:27017/mi_base_de_datos';
+
+app.use(express.json());
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log ('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.use('/users', usersRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
