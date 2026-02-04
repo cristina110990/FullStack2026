63deb160c9f9b110ea50b76dea5df75ffb10328a
cristina110990 <cristina110990@gmail.com>
2026-02-04 19:00:18 +0100

    dia9

--- dia9/read.js
+++ dia9/read.js
@@ -8,3 +8,11 @@ fs.readFile('archivo.txt', 'utf8', (err, data) => { //archivo.txt es el nombre d
         console.log(`Contenido del archivo: ${data}`); // Mostrar el contenido leído en la consola, data contiene el contenido del archivo
     }
 });
+
+fs.readFile('archivoMedio.txt', 'utf8', (err, data) => { //archivo.txt es el nombre del archivo, 'utf8' es la codificación, err es el posible error, data es el contenido leído
+    if (err){
+        console.error(err);
+    } else {
+        console.log(`Contenido del archivo: ${data}`); // Mostrar el contenido leído en la consola, data contiene el contenido del archivo
+    }
+});
--- dia9/unlink.js
+++ dia9/unlink.js
@@ -1,5 +1,6 @@
 const fs = require('fs'); // Importar el módulo 'fs' para trabajar con el sistema de archivos
 const filePath = 'archivo.txt'; // Especificar la ruta del archivo a eliminar
+const fileGrande = 'archivoGrande.txt'; // Especificar la ruta del archivo grande a eliminar
 
 // Eliminar el archivo especificado
 fs.unlink(filePath, err => {
@@ -7,5 +8,13 @@ fs.unlink(filePath, err => {
         console.error(err);
     } else {
         console.log("Archivo eliminado con éxito");
+    }
+});
+
+fs.unlink(fileGrande, err => {
+    if (err){
+        console.error(err);
+    } else {
+        console.log("Archivo grande eliminado con éxito");
     }
 });
