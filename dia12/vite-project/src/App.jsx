9ad2357a403b1ff70a815701cff6b23dc96ba6fa
Cristina Villar Romero <cristina110990@gmail.com>
2026-02-05 18:54:06 +0100

    dia7

--- dia12/vite-project/src/App.jsx
+++ dia12/vite-project/src/App.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState("")
+  const [mensaje, setMensaje] = useState("")
 
   useEffect(() => {
-    fetch("http://localhost:3000/persons")
+    fetch("http://localhost:8080/api/mensaje")
     .then((response) => response.json())
-    .then((data) => setPersons(data.persons))
+    .then((data) => setMensaje(data.mensaje))
     .catch((error) => console.error(`Error al obtener el mensaje: ${error}`))
   }, [])
 
   return (
     <>
       <h1>Mensaje desde Express</h1>
-      <p>{persons}</p>
+      <p>{mensaje}</p>
     </>
   )
 }
